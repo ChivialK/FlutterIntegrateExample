3a2429ad269c2688bc6dd1954bb502732f02f092
RC <cdump1-dev@yahoo.com>
2021-12-28 16:38:39 +0800

    flutter auto_route update

--- flutter_integrate_module/lib/app_router.gr.dart
+++ flutter_integrate_module/lib/app_router.gr.dart
@@ -1,45 +1,49 @@
+// **************************************************************************
+// AutoRouteGenerator
+// **************************************************************************
+
 // GENERATED CODE - DO NOT MODIFY BY HAND
 
 // **************************************************************************
 // AutoRouteGenerator
 // **************************************************************************
 
-import 'package:auto_route/auto_route.dart' as _i1;
-import 'package:flutter/material.dart' as _i2;
+import 'package:auto_route/auto_route.dart' as _i3;
+import 'package:flutter/material.dart' as _i4;
 
-import 'empty_page.dart' as _i4;
-import 'main.dart' as _i3;
+import 'empty_page.dart' as _i2;
+import 'main.dart' as _i1;
 
-class AppRouter extends _i1.RootStackRouter {
-  AppRouter([_i2.GlobalKey<_i2.NavigatorState>? navigatorKey])
+class AppRouter extends _i3.RootStackRouter {
+  AppRouter([_i4.GlobalKey<_i4.NavigatorState>? navigatorKey])
       : super(navigatorKey);
 
   @override
-  final Map<String, _i1.PageFactory> pagesMap = {
-    MyHomeRoute.name: (routeData) => _i1.MaterialPageX<dynamic>(
-        routeData: routeData,
-        builder: (data) {
-          final args = data.argsAs<MyHomeRouteArgs>(
-              orElse: () => const MyHomeRouteArgs());
-          return _i3.MyHomePage(key: args.key);
-        }),
-    EmptyRoute.name: (routeData) => _i1.MaterialPageX<dynamic>(
-        routeData: routeData,
-        builder: (_) {
-          return _i4.EmptyPage();
-        })
+  final Map<String, _i3.PageFactory> pagesMap = {
+    MyHomeRoute.name: (routeData) {
+      final args = routeData.argsAs<MyHomeRouteArgs>(
+          orElse: () => const MyHomeRouteArgs());
+      return _i3.MaterialPageX<dynamic>(
+          routeData: routeData, child: _i1.MyHomePage(key: args.key));
+    },
+    EmptyRoute.name: (routeData) {
+      return _i3.MaterialPageX<dynamic>(
+          routeData: routeData, child: _i2.EmptyPage());
+    }
   };
 
   @override
-  List<_i1.RouteConfig> get routes => [
-        _i1.RouteConfig(MyHomeRoute.name, path: '/'),
-        _i1.RouteConfig(EmptyRoute.name, path: '/empty-page')
+  List<_i3.RouteConfig> get routes => [
+        _i3.RouteConfig(MyHomeRoute.name, path: '/'),
+        _i3.RouteConfig(EmptyRoute.name, path: '/empty-page')
       ];
 }
 
-class MyHomeRoute extends _i1.PageRouteInfo<MyHomeRouteArgs> {
-  MyHomeRoute({_i2.Key? key})
-      : super(name, path: '/', args: MyHomeRouteArgs(key: key));
+/// generated route for
+/// [_i1.MyHomePage]
+class MyHomeRoute extends _i3.PageRouteInfo<MyHomeRouteArgs> {
+  MyHomeRoute({_i4.Key? key})
+      : super(MyHomeRoute.name, path: '/', args: MyHomeRouteArgs(key: key));
 
   static const String name = 'MyHomeRoute';
 }
@@ -47,11 +51,18 @@ class MyHomeRoute extends _i1.PageRouteInfo<MyHomeRouteArgs> {
 class MyHomeRouteArgs {
   const MyHomeRouteArgs({this.key});
 
-  final _i2.Key? key;
+  final _i4.Key? key;
+
+  @override
+  String toString() {
+    return 'MyHomeRouteArgs{key: $key}';
+  }
 }
 
-class EmptyRoute extends _i1.PageRouteInfo {
-  const EmptyRoute() : super(name, path: '/empty-page');
+/// generated route for
+/// [_i2.EmptyPage]
+class EmptyRoute extends _i3.PageRouteInfo<void> {
+  const EmptyRoute() : super(EmptyRoute.name, path: '/empty-page');
 
   static const String name = 'EmptyRoute';
 }
